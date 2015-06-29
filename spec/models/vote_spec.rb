5a94d09bc3f440055b9f11ab655e2c31102db73a
Joeshmoe523 <joeshmoe523@gmail.com>
2015-06-29 16:27:10 -0700

    Completed voting checkpoint

--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -5,7 +5,17 @@ describe Vote do
 
     describe "value validation" do
       it "only allows -1 or 1 as values" do
-        expect( @post.votes.value.valid? ).to eq( true )
+        #Setup
+        @post = Post.create(title: "A sample post", body: "Blah blah blah blah!!")
+        @vote = @post.votes.create(value: 1)
+        @post.votes.create(value: 0)
+        #Execution
+        #Validation
+
+
+        expect( @post.votes.first.value ).to be_in( [1,-1] )
+        expect( @post.votes.first ).to be_valid
+        expect( @post.votes[1] ).to_not be_valid
       end
     end
 
