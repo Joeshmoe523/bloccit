09366742bad6353b9c88dd1d1cce9d72f239762e
Joeshmoe523 <joeshmoe523@gmail.com>
2015-07-01 11:03:22 -0700

    Edited Favorite Mailer

--- app/mailers/favorite_mailer.rb
+++ app/mailers/favorite_mailer.rb
@@ -8,6 +8,12 @@ class FavoriteMailer < ApplicationMailer
     headers["Message-ID"] = "<comments/#{comment.id}@joe-bloccit.example>"
     headers["In-Reply-To"] = "<post/#{post.id}@joe-bloccit.example>"
     headers["References"] = "<post/#{post.id}@joe-bloccit.example>"
+
+    @user = user
+    @post = post
+    @comment = comment
+
+    mail(to: user.email, subject: "New comment on #{post.title}")
     
   end
 
