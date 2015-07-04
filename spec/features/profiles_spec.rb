b38301a19c1e70a4ba3c9cafac80e91067a73638
Joeshmoe523 <joeshmoe523@gmail.com>
2015-07-04 14:54:09 -0700

    Completed Public Profiles assignment

--- spec/features/profiles_spec.rb
+++ spec/features/profiles_spec.rb
@@ -1,28 +1,47 @@
 require 'rails_helper'
+include Warden::Test::Helpers
+Warden.test_mode!
 
 describe "Visiting profiles" do 
 
   include TestFactories
 
-  before do
-    @user = authenticated_user 
-    @post = associated_post(user: @user)
-    @comment = Comment.new(user: @user, post: @post, body: "A Comment")
-    allow(@comment).to receive(:send_favorite_emails)
-    @comment.save!
-  end
-
-  describe "not signed in" do 
-
-    it "shows profile" do 
-      visit user_path(@user)
-      expect(current_path).to eq(user_path(@user))
-
-      expect( page ).to have_content(@user.name)
-      expect( page ).to have_content(@post.title)
-      expect( page ).to have_content(@comment.body)
+    before do
+      @user = authenticated_user 
+      @post = associated_post(user: @user)
+      @comment = Comment.new(user: @user, post: @post, body: "A Comment")
+      allow(@comment).to receive(:send_favorite_emails)
+      @comment.save!
     end
 
-  end
+    describe "not signed in" do 
+
+      it "shows profile" do 
+        visit user_path(@user)
+        expect(current_path).to eq(user_path(@user))
+
+        expect( page ).to have_content(@user.name)
+        expect( page ).to have_content(@post.title)
+        expect( page ).to have_content(@comment.body)
+      end
+
+    end
+
+    describe "visiting user's own profile" do 
+
+      before do
+        login_as(@user, scope: :user)
+      end
+
+      it "shows profile" do 
+        visit user_path(@user)
+        expect(current_path).to eq(user_path(@user))
+
+        expect( page ).to have_content(@user.name)
+        expect( page ).to have_content(@post.title)
+        expect( page ).to have_content(@comment.body)
+      end
+
+    end
 
 end
