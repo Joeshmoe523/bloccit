47a60b8fe41ce75e78a61f802693beb5bb4e2a32
Joeshmoe523 <joeshmoe523@gmail.com>
2015-07-05 18:08:26 -0700

    Completed creating with ajax checkpoint

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,14 +5,18 @@ class CommentsController < ApplicationController
 
     @comment.user = current_user
     @comment.post = @post
+    @new_comment = Comment.new
     
     authorize @comment
     if @comment.save
       flash[:notice] = "Comment was saved."
-      redirect_to topic_post_path(@post.topic, @post)
     else
       flash[:error] = "There was an error saving the comment. Please try again."
-      render :create
+    end
+
+    respond_to do |format|
+      format.html
+      format.js
     end
   end
 
